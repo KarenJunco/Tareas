91a3f5a577871257cc5cd786f44e14e410562c5c
KarenJunco <karenbetianajunco@hotmail.com>
2020-11-23 17:20:10 -0300

    modifique los archivos porque estaban desactualizados

--- tareas.js
+++ tareas.js
@@ -1,21 +1,43 @@
 let fs = require('fs');
+let listaDeTareas = fs.readFileSync('./tareas.json','utf-8');
+let tareas = JSON.parse(listaDeTareas);
+
 module.exports = moduloTareas = {
     archivo : './tareas.json',
     leerJSON : function() {
-        let listaDeTareas = fs.readFileSync(this.archivo,'utf-8');
-        return JSON.parse(listaDeTareas)
+       return tareas
     },
     escribirJSON : function(titulo,estado){
+
         let nuevaTarea = {
             titulo : titulo,
             estado : estado
         }
-        let tareasAnteriores = this.leerJSON();
-        tareasAnteriores.push(nuevaTarea);
-        this.guardarJSON(tareasAnteriores);
+
+        tareas.push(nuevaTarea)
+
+        this.guardarJSON(tareas)
     },
     guardarJSON : function(info){
         let nuevoJSON = JSON.stringify(info);
-        fs.writeFileSync(this.archivo,nuevoJSON,'utf-8')
+        fs.writeFileSync(this.archivo,nuevoJSON,'utf-8');
+        return console.log('El JSON ha sido guardado exitosamente')
+    },
+    deshacer : function(){
+     
+        tareas.pop()
+        this.guardarJSON(tareas)
+    },
+    buscarTarea : function(busqueda){
+
+        let tareasFiltradas = tareas.filter(function(tarea){
+            
+            return tarea.titulo.toLowerCase().includes(busqueda.toLowerCase())
+
+        })
+
+        return tareasFiltradas
+        
     }
+  
 }
